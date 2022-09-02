90b667cf374db979157a29f5dbbaf5fcdb3a0b67
mintusarker <sarkermintu707@gmail.com>
2022-09-03 01:18:44 +0600

    send some data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('rifa')
+const catagory = document.getElementById('catagories');
+const div = document.createElement('div');
+div.classList.add('d-flex', 'justify-content-between','mt-3')
+div.innerHTML = `
+<button class ="btn btn-light fs-5">Home</button>
+<button class ="btn btn-light fs-5">Breaking News</button>
+<button class ="btn btn-light fs-5">Regular News</button>
+<button class ="btn btn-light fs-5">International News</button>
+<button class ="btn btn-light fs-5">Sports</button>
+<button class ="btn btn-light fs-5">Entertainment</button>
+<button class ="btn btn-light fs-5">Culture</button>
+<button class ="btn btn-light fs-5">Arts</button>
+<button class ="btn btn-light fs-5">All News</button>
+
+
+`;
+
+catagory.appendChild(div);
